1e1772884d2e81dc903b7240ada27b0ef8608bae
raniazh <raniazahraoui46@gmail.com>
2025-01-29 21:55:40 +0100

    Adding UserService

---
+++ backend/demo/src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,14 @@
+package com.example.demo.service;
+
+import com.example.demo.dto.ExamDTO;
+import com.example.demo.model.Exam;
+import com.example.demo.repository.ExamRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+@Service
+public class User {
+    
+}
