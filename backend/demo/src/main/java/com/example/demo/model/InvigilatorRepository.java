1a8e3615391cbd300048913ef81044a83c1313fe
raniazh <raniazahraoui46@gmail.com>
2025-01-31 11:48:08 +0100

    Ajout des class entity et repository

---
+++ backend/demo/src/main/java/com/example/demo/model/InvigilatorRepository.java
@@ -0,0 +1,6 @@
+package com.example.demo.model;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InvigilatorRepository extends JpaRepository<Invigilator, Integer> {
+   
+}
---
+++ backend/demo/src/main/java/com/example/demo/model/UtilisateurRepository.java
@@ -0,0 +1,4 @@
+package com.example.demo.model;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UtilisateurRepository extends JpaRepository<Utilisateur, Integer> {}
